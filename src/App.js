cf3af84a101501c7c81297833db3df82a1cca5d8
Pragatheeswaran <kasipragi143@gmail.com>
2020-05-27 10:02:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDot9LH1uJzeAw5CsR7eHDRG81u3LFPLEo",
+    authDomain: "react-auth-padink.firebaseapp.com"
+})
+
+class App extends Component {
+    state = { isSignedIn: false }
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => false
+        }
+    }
+
+    componentDidMount = () => {
+      firebase.auth().onAuthStateChanged(user => {
+        this.setState({ isSignedIn: !!user })
+        console.log("user", user)
+      })
+    }
+
+
+    render() {
+      return (
+        <div className="App">
+          {this.state.isSignedIn ? (
+            <span>
+              <div>Signed In!</div>
+              <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+              <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+              <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
+            </span>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
+        </div>
+      )
+    }
+  }
+
+  export default App;
